30def04e59e8d3a198bcc38249efd37d2a952a05
yuwei <178967037@qq.com>
2018-04-17 10:24:35 +0800

    发现官方confirm T - T

--- reactpc/src/container/testDemo/testDemo1/index.js
+++ reactpc/src/container/testDemo/testDemo1/index.js
@@ -1,20 +1,41 @@
 /**testDemo1 */
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button , Modal} from 'antd';
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux';
+const confirm = Modal.confirm;
 class testDemo1 extends Component {
-
 
   confirm = ()=>{
     console.log('click confirm')
     //告诉redux需要打开confirm表单-并且传入确认时候回调函数
     this.props.toggleConfirm(true,{
       title:'警告！',
-      content:'警告！testDemo1文字哦~',
+      content:'哇！花了这么多功夫写的Confirm,居然发现官方有写好的，好气哦！',
       fn:(data)=>{
         console.log(data);//Confirm返回true
         console.log('可以执行callback内容了！')
+      }
+    })
+  }
+  antConfirm = () =>{
+    confirm({
+      okText:"是的",
+      cancelText:"我没有",
+      title: '生气了吗？',
+      content: `感觉自己写的组件太繁杂了吗？`,
+      onOk: async () => {
+        
+          Modal.success({
+            title: '淡定淡定',
+            content: '学如逆水行舟，不进则退',
+          });
+      },
+      onCancel: () => {
+        Modal.error({
+          title: '淡定淡定',
+          content: '要学会直面自己的内心',
+        });
       }
     })
   }
@@ -22,6 +43,7 @@ class testDemo1 extends Component {
     return (
      <span>
         <Button onClick={this.confirm}>点击这里要达到弹出confirm的效果</Button>
+        <Button onClick={this.antConfirm}>点击这里出发官方Antd - Confirm</Button>
      </span>
     )
   }
